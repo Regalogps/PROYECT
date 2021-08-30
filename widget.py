97e5a4c01af8d2e45b743d31d34c6809cdb27f68
Nico Ferry <87967993+Regalogps@users.noreply.github.com>
2021-08-30 09:01:10 -0500

    Update widget.py

--- widget.py
+++ widget.py
@@ -1,1 +1,6 @@
-sbx = Spinbox(, value=('Frox', 'Fox', 'Boomer', 'Ice', 'JD', 'Grub', '', '', '', '', '', '', '', )
+
+sbx = Spinbox(, value=('Frox', 'Fox', 'Boomer', 'Ice', 'JD',
+                       'Grub', 'Light', 'Aduka', 'Knight', 'Kalsiddon',
+                       'Mage', 'Randomizer', 'Jolteon', 'Turtle', 'Armor',
+                       'Asate', 'Raon', 'Trico', 'Nak', 'Big',
+                       'Dragon1', 'Dragon2')
