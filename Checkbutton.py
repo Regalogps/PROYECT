d11ed859fe91d4ff7bed16e388eff1e7a3bfcbe0
Regalogps <regalogps5@gmail.com>
2021-09-03 21:01:59 -0500

    vvvv

---
+++ Checkbutton.py
@@ -0,0 +1,34 @@
+
+from tkinter import *
+fron tkinter import Checkbutton
+
+class CheckbuttoN(Checkbutton):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.variable = BooleanVar(self)
+        self.configure(variable=self.variable)
+    
+    def checked(self):
+        return self.variable.get()
+    
+    def check(self):
+        self.variable.set(True)
+    
+    def uncheck(self):
+        self.variable.set(False)
+class Application(ttk.Frame):
+    
+    def __init__(self, main_window):
+        super().__init__(main_window)
+        main_window.title("Checkbutton en tkinter")
+        
+        # No es necesario crear una variable.
+        self.checkbox = CheckbuttoN(self,
+            text="Opción", command=self.check_clicked)
+        self.checkbox.place(x=40, y=70)
+        
+        self.place(width=300, height=200)
+    
+    def check_clicked(self):
+        print(self.checkbox.checked())
