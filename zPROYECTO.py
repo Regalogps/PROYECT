fda2e857743805942784cd6ee991cf6aaa2b1fa3
Nico Ferry <87967993+Regalogps@users.noreply.github.com>
2021-09-14 16:17:29 -0500

    nuevo martes 4 17

--- zPROYECTO.py
+++ zPROYECTO.py
@@ -294,7 +294,7 @@ class Interface(Tk):
             print('tambien entro')                                             
             for i in self.spinbox_values:          
                 if value in i:
-                    #self.listbox .config (state= NORMAL)#
+                    #self.listbox .config (state= NORMAL)#aaaaaaa
                     list_new .append(i)
                     print('Agregando a la lista: ', list_new) 
                     
