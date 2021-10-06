a0a716207263724cfbc2ea73f2ce502a6f4cfef5
Nico Ferry <87967993+Regalogps@users.noreply.github.com>
2021-10-06 06:27:25 -0500

    Update SCREEENNN.py

--- SCREEENNN.py
+++ SCREEENNN.py
@@ -6,7 +6,9 @@
     def pantalla_completa(self):
         width = self.master.winfo_screenwidth()
         height = self.master.winfo_screenheight()
-        izq_x =
+        tplvl_w = width * 20 / 100
+        tplvl_h = height - 50
+        
         
         self.ventana .geometry('{0}x{1}+0+0' .format(self.ventana .winfo_screenwidth(),
                      self.ventana .winfo_screenheight () - 30))
