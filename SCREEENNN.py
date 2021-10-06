d35af40b0092e038dd38b4b507cc348b293a185b
Nico Ferry <87967993+Regalogps@users.noreply.github.com>
2021-10-06 05:22:18 -0500

    Create SCREEENNN.py

---
+++ SCREEENNN.py
@@ -0,0 +1,13 @@
+
+
+
+
+
+    def pantalla_completa(self):
+        width = self.master.winfo_screenwidth()
+        height = self.master.winfo_screenheight()
+        izq_x =
+        
+        self.ventana .geometry('{0}x{1}+0+0' .format(self.ventana .winfo_screenwidth(),
+                     self.ventana .winfo_screenheight () - 30))
+   
