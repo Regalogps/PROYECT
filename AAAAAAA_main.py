9a2384f113218829ea34f80f01686eafb3bb8954
Nico Ferry <87967993+Regalogps@users.noreply.github.com>
2021-10-06 15:54:28 -0500

    Update AAAAAAA_main.py

--- AAAAAAA_main.py
+++ AAAAAAA_main.py
@@ -70,8 +70,35 @@ class Interface(Frame):  #--------------------------> FRAME CONTROLADOR PRINCIPA
         self._minimize = False
         
         #_____Métodos Llamados:
-        self. configure_interface()
-        self. widgets()
+        self.size_position()
+        self.configure_interface()
+        self.widgets()
+
+        #_____Variables de Control de tamaño y Posición de todas las ventanas:
+        self._dist_izq = IntVar()
+        self._dist_der = IntVar()
+
+    def size_position(self):
+
+        screen_x = self.master.winfo_screenwidth()
+        screen_y = self.master.winfo_screenheight()
+
+        width_1 = screen_x * 20 / 100   # Ancho de la ventana  Aprox: 20% de 1200 = 220
+        height_1 = screen_y - 50        # Alto de la ventana   Aprox: 800 - 50 = 750
+        pos_x1 = 0                      # Posición en eje x
+        pos_y1 = 20                     # Posición en eje y
+        
+        pos_x2 = width_1 * 4
+
+        width_2 = screen_x *
+        height_2 = screen_y 
+        
+        window_izq = ('{}x{}+{}+{}'.format(width_1, heigth_1, pos_x1, pos_y1))
+        window_der = ('{}x{}+{}+{}'.format(width_1, height_1, pos_x2, pos_y1))
+        window_stuf = ('{}x{}+{}+{}'.format(width_1, height_1, pos_x2, pos_y1))
+   
+        new_pos = "+{}+{}".format(self.winfo_x() + deltax, self.winfo_y() + deltay)
+
 
 
     def configure_interface(self):   # CONFIGURA TOPLEVEL PRINCIPA + FALTAA POSICIONAR LA VENTANA EN INICIACION
