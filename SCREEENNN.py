f9e6446ba5e43499c9ba4fa23cd10958a93d2859
Nico Ferry <87967993+Regalogps@users.noreply.github.com>
2021-10-06 15:40:32 -0500

    Update SCREEENNN.py

--- SCREEENNN.py
+++ SCREEENNN.py
@@ -1,6 +1,6 @@
 
 
-
+        wi
 
 
     def pantalla_completa(self):
@@ -20,8 +20,5 @@
         window_izq = ('{}x{}+{}+{}'.format(width_1, heigth_1, pos_x1, pos_y1))
         window_der = ('{}x{}+{}+{}'.format(width_1, height_1, pos_x2, pos_y1))
         window_stuf = ('{}x{}+{}+{}'.format(width_1, height_1, pos_x2, pos_y1))
-        
-        self.ventana .geometry('{0}x{1}+0+0' .format(self.ventana .winfo_screenwidth(),
-                     self.ventana .winfo_screenheight () - 30))
    
         new_pos = "+{}+{}".format(self.winfo_x() + deltax, self.winfo_y() + deltay)
