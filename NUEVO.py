286395764eedca34110ab7e8e44ed61d18a1191a
Nico Ferry <87967993+Regalogps@users.noreply.github.com>
2021-10-05 23:47:33 -0500

    Update NUEVO.py

--- NUEVO.py
+++ NUEVO.py
@@ -1,41 +1,25 @@
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 from tkinter import *
 
 class A(Frame):  #--------------------------> FRAME CONTROLADOR PRINCIPAL
     def __init__(self, master=None, *args, **kwargs):
         Frame.__init__(self, master, *args, **kwargs)
+        self.frame = Frame(self, bg='yellow')
+        self.frame .pack(side=TOP, fill=BOTH)
+         
+        self.cnt = Frame(self.frame, bg='green2')
+        self.cnt .pack(side=TOP, fill=BOTH)
 
-        self.container_A = Frame(root, bg='green')
-        self.container_A .pack(fill=BOTH)
-        btn = Button(self.container_A, text='__1__')
+        btn = Button(self.cnt, text='Hellow')
         btn.pack()
 
-        self.container_B = Frame(self.container_A, bg='green2')
-        self.container_B .pack(fill=BOTH)
-        btn = Button(self.container_B, text='__2__')
-        btn.pack()
+        self.bind_all('<Motion-1>', self.hellow)
 
-        self.bind_all("<B1-Motion>", self.a)
-
-    def a (self, event):
-        event
+    def hellow(self, event):
+        print('I always run how I want')
 
 root = Tk()
 app = A (root)
-app .pack()
+app .pack(side=TOP, fill=BOTH)
 root.mainloop()
