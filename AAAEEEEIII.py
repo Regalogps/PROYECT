04810a503c74b74f32e90e306f844c8b5072f2dc
Nico Ferry <87967993+Regalogps@users.noreply.github.com>
2021-10-09 00:57:27 -0500

    Update AAAEEEEIII.py

--- AAAEEEEIII.py
+++ AAAEEEEIII.py
@@ -1,3 +1,29 @@
+self.Frog_1 .grid (column= 0, row= 0, pady= 3, padx= (5,0))
+        self.Fox_2 .grid (column= 1, row= 0, pady= 3, padx= (0,0))       
+        self.Boomer_3 .grid (column= 2, row= 0, pady= 3, padx= (0,0))           
+        self.Ice_4 .grid (column= 3, row= 0, pady= 3, padx= (0,0))
+        self.JD_5 .grid (column= 4, row= 0, pady= 3, padx= (0,0))
+        self.Grub_6 .grid (column= 5, row= 0, pady= 3, padx= (0,0))
+        self.Lightning_7 .grid (column= 6, row= 0, pady= 3, padx= (0,0))
+        self.Aduka_8 .grid (column= 7, row= 0, pady= 3, padx= (0,0))
+        self.Knight_9 .grid (column= 8, row= 0, pady= 3, padx= (0,0))
+        self.Kalsiddon_10 .grid (column= 9, row= 0, pady= 3, padx= (0,0))
+        self.Mage_11 .grid (column= 10, row= 0, pady= 3, padx= (0,5))
+
+        self.Randomizer_12 .grid (column= 0, row= 1, pady= 2, padx= (5,0))
+        self.Jolteon_13 .grid (column= 1, row= 1, pady= 2, padx= (0,0))
+        self.Turtle_14 .grid (column= 2, row= 1, pady= 2, padx= (0,0))
+        self.Armor_15 .grid (column= 3, row= 1, pady= 2, padx= (0,0))
+        self.Asate_16 .grid (column= 4, row= 1, pady= 2, padx= (0,0))
+        self.Raon_17 .grid (column= 5, row= 1, pady= 2, padx= (0,0))
+        self.Trico_18 .grid (column= 6, row= 1, pady= 2, padx= (0,0))
+        self.Nak_19 .grid (column= 7, row= 1, pady= 2, padx= (0,0))
+        self.Bigfoot_20 .grid (column= 8, row= 1, pady= 2, padx= (0,0))
+        self.Barney_21 .grid (column= 9, row= 1, pady= 2, padx= (0,0))
+        self.Dragon_22 .grid (column= 10, row= 1, pady= 2, padx= (0,5))
+
+
+
 def bucle (self):
         lst_1 = [['Frog', 'Fox', 'Boomer', 'Ice', 'J.d', 'Grub',
                   'Lightning', 'Aduka', 'Knight', 'Kalsiddon', 'Mage'],
@@ -7,9 +33,11 @@ def bucle (self):
         #lst_2 = [['Fox','Knight','Jolteon','Barney','Dragon']]  
     
         for index, i in enumerate(lst_1):
-            for index2, e in enumerate(i):
-                btn = CButton (self.frame_1, text=e) 
-                btn .grid(columna=index_2 , row=index , pady= , padx=())
+            for indexe, e in enumerate(i):
+                btn = CButton (self.frame_1, text=e)
+                cinco = (5,0) if indexe == 0 else exc = 0
+                btn .grid(columna=indexe , row=index , pady=3-index , padx=exc)
+
 
                 if index < 11 and index != 0 and index != 10:
                     btn .grid(column= index, row= 0, pady= 3, padx= (0,0))               
@@ -26,5 +54,5 @@ def bucle (self):
                     btn .grid (column= 10, row= 1, pady= 2, padx= (0,5))
 
                 if i in e:
-                    btn.config(fg='yellow', activebackground='#ebb015')
+                  btn.config(fg='yellow', activebackground='#ebb015')
     
