12233cbb9b3fd69b718d0917d5945b00d1c426fd
Regalogps <regalogps5@gmail.com>
2021-10-09 12:48:40 -0500

    444ss

--- AAAVVVV.py
+++ AAAVVVV.py
@@ -67,31 +67,29 @@ class Frame1Cls(Frame):
         self.Dragon_22 .grid (column= 10, row= 1, pady= 2, padx= (0,5))
 
     def bucle (self):
-        lst_1 = ['Frog', 'Fox', 'Boomer', 'Ice', 'J.d', 'Grub', 'Lightning', 'Aduka',
-               'Knight','Kalsiddon', 'Mage', 'Randomizer', 'Jolteon', 'Turtle','Armor',
-               'A.sate', 'Raon', 'Trico', 'Nak', 'Bigfoot', 'Barney', 'Dragon']
+        lst_1 = [['Frog', 'Fox', 'Boomer', 'Ice', 'J.d', 'Grub',
+                  'Lightning', 'Aduka', 'Knight', 'Kalsiddon', 'Mage'],
+                 ['Randomizer', 'Jolteon', 'Turtle', 'Armor', 'A.sate',
+                  'Raon', 'Trico', 'Nak', 'Bigfoot', 'Barney', 'Dragon']]
+
         lst_2 = [['Fox','Knight','Jolteon','Barney','Dragon']]  
     
         for index, i in enumerate(lst_1):
-            for index2, e in enumerate(lst_2):
-                btn = CButton (self.frame_1, text=i) 
+            print(i)
+            for dex, x in enumerate(i):
+                for dey in (lst_2):
+                    btn = CButton (self.frame_1, text=x) #command=lambda:self.windows(
+                                 # lambda top1:ShowImage1(top1, index_1, index_2, self.path_lst),
+                                 # lambda top2:ShowImage2(top2, index_1, index_2, self.path_lst),
+                                 # lambda top3:ShowImage3(top3, index_1, index_2, self.path_lst)))         
+                    five = (5,0) if dex == 0 else 0
+                    #print('indice:',dex,'es:', five)
+                    five = (0,5) if dex == 10 else 0
+                    #print('indice:',dex,'es:', five)
+                    btn .grid(column=dex , row=index , pady=3 - index, padx=five)
+                    #print('indice:',dex,'es:', five)
 
-                if index < 11 and index != 0 and index != 10:
-                    btn .grid(column= index, row= 0, pady= 3, padx= (0,0))               
-                if index > 10 and index != 11 and index != 21:
-                    index = index - 11
-                    btn .grid(column= index, row= 1, pady= 2, padx= (0,0)) 
-                if index == 0:
-                    btn .grid (column= 0, row= 0, pady= 3, padx= (5,0))
-                if index == 10:
-                    btn .grid (column= 10, row= 0, pady= 3, padx= (0,5))
-                if index == 11:
-                    btn .grid (column= 0, row= 1, pady= 2, padx= (5,0))
-                if index == 21:  
-                    btn .grid (column= 10, row= 1, pady= 2, padx= (0,5))
-
-                if i in e:
-                    btn.config(fg='yellow', activebackground='#ebb015')
+                    if x in dey: btn.config(fg='yellow', activebackground='#ebb015')
     
                   
 
