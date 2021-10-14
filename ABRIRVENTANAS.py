5164dabfb0ecae400cb58ab791e6ae6fa23283e9
Nico Ferry <87967993+Regalogps@users.noreply.github.com>
2021-10-13 22:12:08 -0500

    Create ABRIRVENTANAS.py

---
+++ ABRIRVENTANAS.py
@@ -0,0 +1,27 @@
+    def listbox_enter(self, event):  # ACTIVA: CON TECLA ENTER - INSERTA EL VALOR DE LISTBOX A SPINBOX, MANDA LLAMAR A OPEN_WINDOWS  Y ABREN LAS VENTANAS
+ 
+        listbx = self.listboxx.get(0)
+        spinbx = self.spinboxx.get()
+
+        if listbx != spinbx and listbx != '':
+            self.spinboxx.delete(0, END)
+            self.spinboxx.insert(0, listbx)
+   
+        self.open_windows() 
+         
+    def open_windows(self, event=None):  # ACTIVA: ** SI ES LLAMADO POR LISTBOX_SELECT ** - ABRE LAS VENTANAS
+        
+        left = [Frog_left_off, Fox_left_off, Boomer_left_off, Ice_left_off, Jd_left_off, Grub_left_off, Lightning_left_off, Aduka_left_off, Knight_left_off, Kalsiddon_left_off, Mage_left_off, Randomizer_left_off, Jolteon_left_off, Turtle_left_off, Armor_left_off, Asate_left_off, Raon_left_off, Trico_left_off, Nak_left_off, Bigfoot_left_off, Barney_left_off, Dragon_left_off,]
+        right = [Frog_right, Fox_right, Boomer_right, Ice_right, Jd_right, Grub_right, Lightning_right, Aduka_right, Knight_right, Kalsiddon_right, Mage_right, Randomizer_right, Jolteon_right, Turtle_right, Armor_right, Asate_right, Raon_right, Trico_right, Nak_right, Bigfoot_right, Barney_right, Dragon_right]
+        stuf = [Frog_stuf, Fox_stuf, Boomer_stuf, Ice_stuf, Jd_stuf, Grub_stuf, Lightning_stuf, Aduka_stuf, Knight_stuf, Kalsiddon_stuf, Mage_stuf, Randomizer_stuf, Jolteon_stuf, Turtle_stuf, Armor_stuf, Asate_stuf, Raon_stuf, Trico_stuf, Nak_stuf, Bigfoot_stuf, Barney_stuf, Dragon_stuf]
+
+        for index, i in enumerate(self.spinbox_values):     
+            if self.spinbox_variable.get() == i:            # ANTES DABA ERROR CON: self.spinboxx .!toplvel.!frame,etc
+                self.master.windows_123(left[index], right[index], stuf[index]) 
+                break                                       # Sin breack el programa seguiria buscando coincidencias despues del enter, y guardaria un error           
+  
+        if self._change is not None:
+            self.after(10000, self.automatic_deletion) 
+
+    def automatic_deletion(self):  # ACTIVA: ** SI ES LLAMADO POR OPEN_WINDOWS ** Y SI LA VARIABLE DE CONTROL NO ES NONE - LIMPIA SPINBOX
+        self.spinboxx .delete(0, END)     
