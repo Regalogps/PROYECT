129b7cc87ff38a23af741248cd058921c8b9516a
Regalogps <regalogps5@gmail.com>
2021-10-16 21:56:56 -0500

    444444555

--- P22PREEE.py
+++ P22PREEE.py
@@ -5,7 +5,7 @@ class TypeButton(Button):
     def __init__(self, parent, *args, **kwargs):
         kwargs = {'font':('Calibri',9,'bold'), 'bg': '#11161d', 'fg':'white',
                   'width':10, 'bd':0, 'activebackground':'#bdfe04', **kwargs}
-        super().__init__(master, *args, **kwargs)
+        super().__init__(parent, *args, **kwargs)
 
 class Example(Frame):
     def __init__(self,parent, *args, **kwargs):
@@ -16,9 +16,9 @@ class Example(Frame):
 
     def creator_buttons(self):  
 
-        mobiles = [['_0_', '_1_', '_2_', '_3_', '_4_', '_5_', '_6_', '_7_', '_8_', '_9_', '_10_', '_11_'],
-                   ['_12_', '_13_', '_14_', '_15_', '_16_', '_17_', '_18_', '_19_', '_20_', '_21_']]
-        mobiles2 = ['_1_', '_8_', '_12_', '_20_', '_21_'] 
+        mobiles = [['Frog', 'Fox', 'Boomer', 'Ice', 'J.d', 'Grub', 'Lightning', 'Aduka', 'Knight', 'Kalsiddon', 'Mage'],
+                   ['Randomizer', 'Jolteon', 'Turtle', 'Armor', 'A.sate', 'Raon', 'Trico', 'Nak', 'Bigfoot', 'Barney', 'Dragon']]                  
+        mobiles2 = ['Fox','Knight','Jolteon','Barney','Dragon'] 
         buttons = []
         for index1, mobil in enumerate(mobiles):
             for index2, texto in enumerate(mobil):
@@ -33,10 +33,10 @@ class Example(Frame):
                 #btn.bind("<Button-1>", self.mouse_clic)   
                 buttons.append(btn)
 
-    def mouse_move(self, event):   # Change the color when hovering the mouse over the    # Color: Light blue off
+    def mouse_move(self, event):
         event.widget.config(bg="#24364a")
 
-    def mouse_stop(self, event):   # Leave the color as it was by default                 # Color: Dark blue     
+    def mouse_stop(self, event): 
         event.widget.config(bg='#11161d')
     
     #def mouse_clic(self, event):
