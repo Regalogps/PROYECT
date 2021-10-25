021df110a34954517b844b96c5a6b0f16c564971
Regalogps <regalogps5@gmail.com>
2021-10-24 21:17:19 -0500

    888888

--- PROBANDO.py
+++ PROBANDO.py
@@ -764,20 +764,18 @@ class TopDerCls(Frame):
         # Configuración de la ventana:    
         self.grid_columnconfigure (0,weight=1)
         self.grid_rowconfigure (0,weight=1)
+        
 
 
     #___< B U T T O N - 1 >
     def open_text_flecha(self, event): 
 
         # Convierte el tamaño total de la ventana en porcentaje:  100 %
-        self.porcentage_total_x  = event.x / self.master.winfo_width() * 100              # ---> winfo_width() : Devuelve el ancho actual del widget(Toplevel) en pixeles
+        self.porcentage_total_x = event.x / self.master.winfo_width() * 100              # ---> winfo_width() : Devuelve el ancho actual del widget(Toplevel) en pixeles
         self.porcentage_total_y = event.y / self.master.winfo_height() * 100              # ---> event.x/y     : Devuelve la posicion del mouse en pixeles (click/movimiento)
         x1, x2 = 0, 100
-        y1, y2 = 68, 100  
-        #print('width_',self.pointer_width)
-        #print('height_',self.pointer_height) 
-        #print('xxxx_',event.x )
-        #print('yyyy_',event.y )     
+        y1, y2 = 67, 100  
+      
         if x1 <(self.porcentage_total_x)< x2  and  y1 <(self.porcentage_total_y)< y2: 
 
             if not self._open1 == True:                                               # ---> Si self.test es Falso:   ---> Predeterminado: False
@@ -787,6 +785,8 @@ class TopDerCls(Frame):
                 self.frame_image_base_77 .grid(column=0, row=0)                      # Posiciona                  # == {} (no mapeado)  
                 self.lbl_text_flecha     .grid(column=0, row=0, sticky=SE, ipadx=5) # VER SI ACEPTA VARIABLES
 
+                self.lbl_text_mostrar_77 .grid_forget()
+
                 #print('se cambio de --ON-- a --OF-- ')              
             else:
                 #print('se cambio de --OF-- a --ON-- ')
@@ -794,6 +794,8 @@ class TopDerCls(Frame):
 
                 self.frame_image_base_77 .grid_forget()
                 self.after(0, lambda e = self.motion: self.motion.set('on'))  ## analizae
+
+                self.lbl_text_mostrar_77 .grid (column=0, row=0, sticky=N, ipadx=5, ipady=5)
                 
                 self.lbl_text_flecha.grid_forget()
 
@@ -804,7 +806,8 @@ class TopDerCls(Frame):
         self.pointer_width_2 = event.x / self.master.winfo_width() * 100
         self.pointer_height_2 = event.y / self.master.winfo_height() * 100
         x1, x2 = 0, 100
-        y1, y2 = 68, 100
+        y1, y2 = 67, 100
+        
         if self.motion.get()=='on':    
             if x1 < (self.pointer_width_2) < x2  and  y1 < (self.pointer_height_2) < y2:        
                 self.lbl_text_mostrar_77 .grid (column=0, row=0, sticky=N, ipadx=5, ipady=5)
